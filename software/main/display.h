91e6d922ff4d463573965d102da4c0a4105cc88a
Ben <andreabenini@users.noreply.github.com>
2025-04-15 00:01:42 +0200

    removing start button as direct key

--- software/main/display.h
+++ software/main/display.h
@@ -5,9 +5,6 @@
 #include <driver/gpio.h>
 
 
-// Define GPIO for the buttons
-#define GPIO_BUTTON_START       GPIO_NUM_10         // GPIO 10  (last gpio left, everything else on I2C external bus)
-
 // Define your GPIO pin assignments
 #define LCD_SCK_PIN   GPIO_NUM_4
 #define LCD_MOSI_PIN  GPIO_NUM_6
