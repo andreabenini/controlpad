4f038825d4c518f1859028cd27cdf97d0c11e1a1
Ben <andreabenini@users.noreply.github.com>
2025-04-14 19:58:26 +0200

    placeholders for two new tasks

--- software/main/main.c
+++ software/main/main.c
@@ -1,6 +1,8 @@
 // Program Includes
 #include "main.h"
+#include "display.h"
 #include "keyboard.h"
+#include "communication.h"
 
 
 // System includes
@@ -14,6 +16,8 @@
 void app_main(void) {
     ESP_LOGW(TAG_APP, "Program Started");
     // Create the I/O peripherals management task
-    xTaskCreate(taskKeyboard, "taskKeyboard", 4096, NULL, 5, NULL);
+    // xTaskCreate(taskDisplay,        "taskDisplay",          2048, NULL, 5, NULL);
+    xTaskCreate(taskKeyboard,       "taskKeyboard",         4096, NULL, 5, NULL);
+    // xTaskCreate(taskCommunication,  "taskCommunication",    2048, NULL, 5, NULL);
     return;
 } /**/
