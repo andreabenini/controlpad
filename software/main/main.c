7de5cb585639cec0f1c00da752c4b8fb480be71a
Ben <andreabenini@users.noreply.github.com>
2025-04-29 08:14:58 +0200

    Adding a serial task for dealing with configurations uploads

--- software/main/main.c
+++ software/main/main.c
@@ -1,6 +1,7 @@
 // Program Includes
 #include "main.h"
 #include "mainloop.h"
+#include "serial.h"
 
 
 // System includes
@@ -8,11 +9,11 @@
 #include "freertos/task.h"
 #include <esp_log.h>
 #include <esp_system.h>
-#include <nvs_flash.h>
 
 
 void app_main(void) {
     ESP_LOGW(TAG_APP, "Program Started");
+
     // Create the I/O peripherals management task
     //      - taskFunction
     //      - task name for debugging
@@ -20,5 +21,7 @@ void app_main(void) {
     //      - task parameters
     //      - task priority (lower number = lower priority)
     //      - task handle (optional)
-    xTaskCreate(taskMainLoop, "taskLoop", 4096, NULL, 5, NULL);
+    xTaskCreate(taskMainLoop, "taskLoop",   4096, NULL, 5, NULL);
+    // Create serial task for receiving async incoming configurations from console (I've a 10K buffer for data stream in it)
+    xTaskCreate(taskSerial,   "taskSerial", 4096, NULL, 12, NULL);
 } /**/
