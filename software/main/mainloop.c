3449da42e7ee3879df03f1c30e3b1efcec154454
Ben <andreabenini@users.noreply.github.com>
2025-05-01 00:28:54 +0200

    main loop managing the FSM machine and the entire project

---
+++ software/main/mainloop.c
@@ -0,0 +1,195 @@
+#include "mainloop.h"
+#include "freertos/projdefs.h"
+#include "i2c.h"
+#include "joystick.h"
+#include "fsm.h"
+#include "configuration.h"
+
+
+
+keyboardStatus keyboard = {0};
+extern QueueHandle_t queueMessage;
+
+
+/**
+ * buttonRead - Collect button events from I2C
+ * @param gpioA_data (uint8_t) GPIO-A register from MCP23017
+ * @param gpioB_data (uint8_t) GPIO-B register from MCP23017
+ * @return (void)
+ */
+void buttonRead(uint8_t gpioA_data, uint8_t gpioB_data) {
+    uint8_t  i;
+    bool     stateRaw;
+    uint32_t timeCurrent = pdTICKS_TO_MS(xTaskGetTickCount());
+    
+    // Buttons are connected to ground and pull-ups are enabled,
+    // Invert and mask to get pressed states, 0=pressed, 1=not pressed
+    gpioA_data = ~gpioA_data;
+    gpioB_data = ~gpioB_data;
+    // GPIO-A (0-7) Process buttons, if needed
+    for (i=0; i<8; i++) {
+        stateRaw = (gpioA_data & (1<<i)) > 0;
+        // If stateRaw changed from latest reading
+        if (stateRaw != keyboard.button[i].stateLastRaw) {
+            keyboard.button[i].stateLastRaw   = stateRaw;
+            keyboard.button[i].timeLastChange = timeCurrent;
+        } else {
+            // If enough time has passed since the last change, update the current state
+            if ((timeCurrent - keyboard.button[i].timeLastChange >= TIME_DEBOUNCE_BUTTONS) && (keyboard.button[i].stateCurrent != stateRaw)) {
+                keyboard.button[i].stateCurrent = stateRaw;
+                eventButton(i, stateRaw);
+            }
+        }
+    }
+    // GPIO-B (8-15) Process buttons, if needed
+    for (i=0; i<8; i++) {
+        stateRaw = (gpioB_data & (1<<i)) > 0;
+        // If stateRaw changed from latest reading
+        if (stateRaw != keyboard.button[i+8].stateLastRaw) {
+            keyboard.button[i+8].stateLastRaw   = stateRaw;
+            keyboard.button[i+8].timeLastChange = timeCurrent;
+    
+        } else {
+            // If enough time has passed since the last change, update the current state
+            if ((timeCurrent - keyboard.button[i+8].timeLastChange >= TIME_DEBOUNCE_BUTTONS) && (keyboard.button[i+8].stateCurrent != stateRaw)) {
+                keyboard.button[i+8].stateCurrent = stateRaw;
+                eventButton(i+8, stateRaw);
+            }
+        }
+    }
+} /**/
+
+
+/**
+ * Main task function for keyboard management
+ */
+void taskMainLoop(void *pvParameter) {
+    ESP_LOGI(TAG_MAINLOOP, "Task created");
+    esp_err_t               err;
+    adc_channel_t           channel[4] = {JOYSTICK1_X, JOYSTICK1_Y, JOYSTICK2_X, JOYSTICK2_Y};
+    adc_continuous_handle_t adcHandle = NULL;
+    uint32_t                adcResultNum = 0;               // Continuous read returned bytes
+    uint8_t                 adcData[ADC_BUFFER_SIZE] = {0}; // Continuous read adc data stream
+    adc_digi_output_data_t  *p;                             // data stream pointer
+    i2c_master_bus_handle_t handleBus;
+    i2c_master_dev_handle_t handleDevice;
+    // Init ADC on axes
+    err = joystickInit(&channel, &adcHandle);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG_MAINLOOP, "Failed to initialize joystick configuration (%d)", err);
+        vTaskDelete(NULL);
+        return;
+    }
+    // I2C
+    err = i2c_MasterInit(&handleBus, &handleDevice);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG_MAINLOOP, "Failed to initialize I2C Master");
+        vTaskDelete(NULL);
+        return;
+    }
+    // Initialize buttons on I2C gpio extender
+    err = mcp23017_Init(handleDevice);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG_MAINLOOP, "Failed to initialize I2C buttons (0x%04X), check I2C connections, program aborted", err);
+        i2c_master_bus_rm_device(handleDevice);
+        i2c_del_master_bus(handleBus);
+        vTaskDelete(NULL);
+        return;
+    }
+
+    // [[LOOP]] // Read controller status, endless loop for the task
+    uint8_t gpioA_data, gpioB_data;
+    if (configurationInit() != ESP_OK) {
+        ESP_LOGE(TAG_MAINLOOP, "Failed to load configuration from the memory");
+        i2c_master_bus_rm_device(handleDevice);
+        i2c_del_master_bus(handleBus);
+        vTaskDelete(NULL);
+        return;
+    }
+    if (statusInit() != ESP_OK) {
+        ESP_LOGE(TAG_MAINLOOP, "Failed to initialize FSM modeInit()");
+        i2c_master_bus_rm_device(handleDevice);
+        i2c_del_master_bus(handleBus);
+        vTaskDelete(NULL);
+        return;
+    }
+    uint32_t   receivedSignal;
+    BaseType_t receivedQueue;
+    while (1) {
+        // Read input from I2C, Register A
+        err = mcp23017_RegisterRead(handleDevice, MCP23017_GPIOA, &gpioA_data);
+        if (err != ESP_OK) {
+            ESP_LOGE(TAG_MAINLOOP, "Failed to read GPIO buttons, Register A");
+            // Clean up before returning
+            i2c_master_bus_rm_device(handleDevice);
+            i2c_del_master_bus(handleBus);
+            vTaskDelete(NULL);
+            return;
+        }
+        // Read input from I2C, Register B
+        err = mcp23017_RegisterRead(handleDevice, MCP23017_GPIOB, &gpioB_data);
+        if (err != ESP_OK) {
+            ESP_LOGE(TAG_MAINLOOP, "Failed to read GPIO buttons, Register B");
+            // Clean up before returning
+            i2c_master_bus_rm_device(handleDevice);
+            i2c_del_master_bus(handleBus);
+            vTaskDelete(NULL);
+            return;
+        }
+        // Read analog values from all joystick axes
+        err = adc_continuous_read(adcHandle, adcData, ADC_BUFFER_SIZE, &adcResultNum, 0);
+        if (err == ESP_OK) {
+            for (int i=0; i<adcResultNum; i+=SOC_ADC_DIGI_RESULT_BYTES) {
+                p = (adc_digi_output_data_t*)&adcData[i];
+                // Check the channel number validation, the data is invalid if the channel num exceed the maximum channel
+                switch (ADC_GET_CHANNEL(p)) {
+                    case 0:
+                        keyboard.joystick1_X = ADC_GET_DATA(p);
+                        break;
+                    case 1:
+                        keyboard.joystick1_Y = ADC_GET_DATA(p);
+                        break;
+                    case 2:
+                        keyboard.joystick2_X = ADC_GET_DATA(p);
+                        break;
+                    case 3:
+                        keyboard.joystick2_Y = ADC_GET_DATA(p);
+                        break;
+                }
+            }
+        } else if (err == ESP_ERR_TIMEOUT) {
+            // Try to read `ADC_BUFFER_SIZE` until API returns timeout, which means there's no available data. Usually "break;"
+            ESP_LOGW(TAG_MAINLOOP, "ADC Continuous read timeout");
+        }
+
+        // Read button functions and trigger events
+        buttonRead(gpioA_data, gpioB_data);             // Read buttons and raise events, if any
+        eventStatus();                                  // Raise event to report overall system state
+        /**
+         * Because printing is slow every time `ulTaskNotifyTake` is called it will immediately return.
+         * To avoid a task watchdog timeout, adding a delay here. When you replace the way you process the data,
+         * usually you don't need this delay (as this task will block for a while).
+         */
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);        // Adding it to avoid task's watchdog timeout (see note above)
+        // Non-blocking check for item in the queue from Task2
+        receivedQueue = xQueueReceive(
+                    queueMessage,                       // The queue to receive from
+                    &receivedSignal,                    // Pointer to where received data will be stored
+                    0                                   // Don't wait (non-blocking)
+                );
+        if (receivedQueue == pdTRUE) {
+            statusChange(STATUS_RELOADCONFIG);
+        }
+        vTaskDelay(pdMS_TO_TICKS(TIME_POLL_DELAY));     // POLL_DELAY between reads (as .h define)
+    }
+    // Clean up
+    // TODO: Add these twos in every exit condition or rearrange exit cleanly
+    ESP_ERROR_CHECK(adc_continuous_stop(adcHandle));
+    ESP_ERROR_CHECK(adc_continuous_deinit(adcHandle));
+
+    i2c_master_bus_rm_device(handleDevice);
+    i2c_del_master_bus(handleBus);
+    ESP_LOGI(TAG_MAINLOOP, "I2C bus closed");
+    ESP_LOGI(TAG_MAINLOOP, "Task closed");
+    vTaskDelete(NULL);                                  // Delete the task, without this I'm getting a guru meditation error with core0 in panic mode
+} /**/
---
+++ software/main/mainloop.h
@@ -0,0 +1,107 @@
+#ifndef MAINLOOP__H
+#define MAINLOOP__H
+
+// System includes
+#include "freertos/FreeRTOS.h"                      // IWYU pragma: keep
+#include <stdint.h>
+#include <stdbool.h>
+#include <esp_err.h>
+#include <esp_log.h>
+#include <driver/gpio.h>                            // IWYU pragma: keep
+#include "driver/i2c_master.h"                      // IWYU pragma: keep
+
+
+// Define GPIO for the buttons
+#define BUTTON_I2C_MASK         0xFF                // Mask for buttons on A0-A3
+#define BUTTON_I2C_NUM          16                  // Digital buttons on I2C this module is handling
+#define TIME_POLL_DELAY         10                  // Delay between two reads
+#define TIME_DEBOUNCE_BUTTONS   50                  // Debounce time in milliseconds, should be greater than TIME_POLL_DELAY (ie: 10, 50)
+// Define Analog Joysticks
+#define DEFAULT_VREF            1100
+#define JOYSTICK1_X             ADC_CHANNEL_0       // GPIO0
+#define JOYSTICK1_Y             ADC_CHANNEL_1       // GPIO1
+#define JOYSTICK2_X             ADC_CHANNEL_2       // GPIO2
+#define JOYSTICK2_Y             ADC_CHANNEL_3       // GPIO3
+#define ADC_ATTEN               ADC_ATTEN_DB_12     // Set attenuation to 11dB (0<->3.3V)  [ADC_ATTEN_DB_11 is now deprecated]
+#define ADC_WIDTH               SOC_ADC_DIGI_MAX_BITWIDTH   // Set width to 12-bit for values between 0 and 4095
+#define ADC_BUFFER_SIZE         60                  // Buffer size for continous ADC readings (60 works)
+#define ADC_CONV_MODE           ADC_CONV_SINGLE_UNIT_1
+
+// I2C Bus setup
+#define I2C_MASTER              I2C_NUM_0           // I2C master port number
+#define I2C_SDA                 8                   // gpio number for I2C master data
+#define I2C_SCL                 9                   // gpio number for I2C master clock
+#define I2C_FREQ_HZ             400000              // I2C master clock frequency (400 KHz)
+#define I2C_TIMEOUT_MS          1000
+#define MCP23017_ADDR           0x20                // MCP23017 I2C address
+#define MCP23017_IODIRA         0x00                // MCP23017 IODIRA register address
+#define MCP23017_IODIRB         0x01                // MCP23017 IODIRB register address
+#define MCP23017_GPIOA          0x12                // MCP23017 GPIOA register address
+#define MCP23017_GPIOB          0x13                // MCP23017 GPIOB register address
+#define MCP23017_REG_GPPUA      0x0C                // Pull-up resistor register A
+#define MCP23017_REG_GPPUB      0x0D                // Pull-up resistor register B
+
+
+#if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
+#define ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE1
+#define ADC_GET_CHANNEL(p_data)     ((p_data)->type1.channel)
+#define ADC_GET_DATA(p_data)        ((p_data)->type1.data)
+#else
+#define ADC_OUTPUT_TYPE             ADC_DIGI_OUTPUT_FORMAT_TYPE2
+#define ADC_GET_CHANNEL(p_data)     ((p_data)->type2.channel)
+#define ADC_GET_DATA(p_data)        ((p_data)->type2.data)
+#endif
+
+
+#define B_A                 0                       // A,B,X,Y  Using classic PS layout
+#define B_B                 1
+#define B_X                 2
+#define B_Y                 3
+#define B_MENU              4                       // Menu button, just below the start button B_START
+#define B_OPTION            5                       // Top right options button
+#define B_RIGHT_TOP         6                       // R1 - Right Top button
+#define B_RIGHT_BOTTOM      7                       // R2 - Right Bottom button
+#define B_DPAD_LEFT         8                       // Left DPAD (left, right, up, down)
+#define B_DPAD_RIGHT        9
+#define B_DPAD_UP           10
+#define B_DPAD_DOWN         11
+#define B_SELECT            12                      // Top left select button
+#define B_LEFT_TOP          13                      // L1 - Left Top button
+#define B_LEFT_BOTTOM       14                      // L2 - Left Bottom button
+#define B_START             15                      // Middle start button (system button)
+
+#define B_MENU_UP           B_A                     // Navigation keys on menu selections (profile or configuration menu)
+#define B_MENU_DOWN         B_B
+#define B_MENU_CONFIRM      B_X
+#define B_MENU_CANCEL       B_Y
+
+#define B_SYSTEM_CONFIG     B_X                     // Hot keys valid in the system configuration menu
+#define B_SYSTEM_TEST       B_B
+#define B_SYSTEM_CANCEL     B_Y
+
+
+// button struct to handle debounce state for each button
+typedef struct {
+    bool            stateCurrent,                   // Current debounced state
+                    stateLastRaw;                   // Last raw state read
+    uint32_t        timeLastChange;                 // Time of last state change
+} button_state_t;
+// Keyboard Status structure
+typedef struct {
+    button_state_t  button[BUTTON_I2C_NUM];         // I2C Buttons (physically handled from the board)
+    uint16_t        joystick1_X,                    // Left joystick
+                    joystick1_Y,
+                    joystick2_X,                    // Right joystick
+                    joystick2_Y;
+} keyboardStatus;
+
+
+
+// Main tasks for the keyboard management
+void taskMainLoop(void *pvParameter);
+void buttonRead(uint8_t gpioA_data, uint8_t gpioB_data);
+
+
+#define TAG_MAINLOOP    "loop"
+
+#endif
