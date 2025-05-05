85b3e5f680c1839c977f7c40430fd2bc7798f824
Ben <andreabenini@users.noreply.github.com>
2025-05-05 08:22:43 +0200

    Configuration functions for dealing with: listing, reading, writing and clearing

--- software/main/configuration.c
+++ software/main/configuration.c
@@ -1,12 +1,13 @@
 #include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include "esp_err.h"
 #include "freertos/FreeRTOS.h"                                  // IWYU pragma: keep
-#include "esp_system.h"                                         // IWYU pragma: keep
-#include "nvs_flash.h"
 #include "nvs.h"
+#include "nvs_flash.h"
 #include "esp_log.h"
+#include "esp_system.h"                                         // IWYU pragma: keep
 
 #include "configuration.h"
 
@@ -30,40 +31,159 @@ esp_err_t configurationInit() {
 } /**/
 
 
+esp_err_t configurationClear() {
+    // [nvsHandle] init
+    nvs_handle_t nvsHandle;
+    esp_err_t result = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &nvsHandle);
+    if (result != ESP_OK) {
+        ESP_LOGE(TAG_CONFIGURATION, "Error opening NVS: %s", esp_err_to_name(result));
+        return result;
+    }
+    // Storing the size of the array
+    result = nvs_set_u8(nvsHandle, NVS_ARRAY_SIZE, 0);
+    if (result != ESP_OK) {
+        ESP_LOGE(TAG_CONFIGURATION, "Cannot write the array size: %s", esp_err_to_name(result));
+        nvs_close(nvsHandle);
+        return result;
+    }
+    ESP_LOGI(TAG_CONFIGURATION, "    - Profile configuration cleared from NVS");
+    return ESP_OK;
+} /**/
+
+
+esp_err_t configurationSave(const profiles *profileArray, uint8_t sizeArray) {
+    if (sizeArray==0) {
+        return configurationClear();
+    }
+    // [nvsHandle] init
+    nvs_handle_t nvsHandle;
+    esp_err_t result = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &nvsHandle);
+    if (result != ESP_OK) {
+        ESP_LOGE(TAG_CONFIGURATION, "Error opening NVS: %s", esp_err_to_name(result));
+        return result;
+    }
+    // Storing the size of the array
+    result = nvs_set_u8(nvsHandle, NVS_ARRAY_SIZE, sizeArray);
+    if (result != ESP_OK) {
+        ESP_LOGE(TAG_CONFIGURATION, "Cannot write the array size: %s", esp_err_to_name(result));
+        nvs_close(nvsHandle);
+        return result;
+    }
+    // Storing the array as a data blob
+    result = nvs_set_blob(nvsHandle, NVS_ARRAY, profileArray, sizeArray*sizeof(profiles));
+    if (result != ESP_OK) {
+        ESP_LOGE(TAG_CONFIGURATION, "Error writing data array to NVS: %s", esp_err_to_name(result));
+        nvs_close(nvsHandle);
+        return result;
+    }
+    // NVS commit
+    result = nvs_commit(nvsHandle);
+    if (result != ESP_OK) {
+        ESP_LOGE(TAG_CONFIGURATION, "Error while committing NVS updates: %s", esp_err_to_name(result));
+        return result;
+    }
+    nvs_close(nvsHandle);
+    ESP_LOGI(TAG_CONFIGURATION, "    - Configuration saved in the NVS area");
+    return ESP_OK;
+} /**/
+
+
 /**
  * Return a list with all available configurations from storage
  */
-esp_err_t configurationList(char names[][CONFIG_LEN_NAME], size_t *count) {
+esp_err_t configurationList(char names[][CONFIG_LEN_NAME], uint8_t *count) {
     // Initialize NVS
     *count = 0;
-    nvs_handle_t nvs_handle;
-    esp_err_t    result = nvs_open(STORAGE_NAMESPACE, NVS_READONLY, &nvs_handle);
+    nvs_handle_t nvsHandle;
+    esp_err_t    result = nvs_open(STORAGE_NAMESPACE, NVS_READONLY, &nvsHandle);
     if (result != ESP_OK) {
-        ESP_LOGW(TAG_CONFIGURATION, "Error opening NVS namespace: %s", esp_err_to_name(result));
+        ESP_LOGE(TAG_CONFIGURATION, "Error opening NVS: %s", esp_err_to_name(result));
         return result;
     }
     // Get number of available configurations
-    size_t used_entries;
-    result = nvs_get_used_entry_count(nvs_handle, &used_entries);
+    result = nvs_get_u8(nvsHandle, NVS_ARRAY_SIZE, count);
     if (result != ESP_OK) {
         ESP_LOGE(TAG_CONFIGURATION, "Error getting entries count: %s", esp_err_to_name(result));
-        nvs_close(nvs_handle);
+        nvs_close(nvsHandle);
         return result;
     }
-    // Listing all keys in NVS
-    nvs_iterator_t iterator = NULL;
-    nvs_entry_info_t info;
-    result = nvs_entry_find(STORAGE_NAMESPACE, NULL, NVS_TYPE_BLOB, &iterator);
-    while (result == ESP_OK && *count < CONFIG_MAX-1) {
-        nvs_entry_info(iterator, &info);
-        if (info.type == NVS_TYPE_BLOB) {
-            strncpy(names[*count], info.key, CONFIG_LEN_NAME-1);
-            names[*count][CONFIG_LEN_NAME-1] = '\0';
-            *count += 1;
-        }
-        result = nvs_entry_next(&iterator);
+    // Listing all configuration names (keys) in NVS
+    size_t configurationsSize;
+    result = nvs_get_blob(nvsHandle, NVS_ARRAY, NULL, &configurationsSize);
+    if (result != ESP_OK) {
+        ESP_LOGE(TAG_CONFIGURATION, "Error getting the size of the data array from NVS: %s", esp_err_to_name(result));
+        nvs_close(nvsHandle);
+        return result;
     }
-    nvs_release_iterator(iterator);
-    nvs_close(nvs_handle);
+    *count = configurationsSize / sizeof(profiles);
+    profiles *configurations  = (profiles *)malloc(configurationsSize);
+    if (configurations == NULL) {
+        ESP_LOGE(TAG_CONFIGURATION, "Cannot allocate memory for %zu", *count);
+        return ESP_ERR_NO_MEM;
+    }
+    result = nvs_get_blob(nvsHandle, NVS_ARRAY, configurations, &configurationsSize);
+    if (result != ESP_OK) {
+        ESP_LOGE(TAG_CONFIGURATION, "Error getting data array from NVS: %s", esp_err_to_name(result));
+        free(configurations);
+        nvs_close(nvsHandle);
+        return result;
+    }
+    nvs_close(nvsHandle);
+    // Iterating available configuration profiles
+    ESP_LOGI(TAG_CONFIGURATION, "Profile list [%d]", *count);
+    for (size_t i=0; i<*count; i++) {
+        strncpy(names[i], configurations[i].name, CONFIG_LEN_NAME-1);
+        names[i][CONFIG_LEN_NAME-1] = '\0';
+        ESP_LOGI(TAG_CONFIGURATION, "    [%s]", names[i]);
+    }
+    free(configurations);
+    return ESP_OK;
+} /**/
+
+
+esp_err_t configurationLoad(profiles *profile, uint8_t profileNumber) {
+    // Initialize NVS
+    nvs_handle_t nvsHandle;
+    esp_err_t    result = nvs_open(STORAGE_NAMESPACE, NVS_READONLY, &nvsHandle);
+    if (result != ESP_OK) {
+        ESP_LOGE(TAG_CONFIGURATION, "Error opening NVS: %s", esp_err_to_name(result));
+        return result;
+    }
+    // Get number of available configurations
+    uint8_t count;
+    result = nvs_get_u8(nvsHandle, NVS_ARRAY_SIZE, &count);
+    if (result != ESP_OK) {
+        ESP_LOGE(TAG_CONFIGURATION, "Error getting entries count: %s", esp_err_to_name(result));
+        nvs_close(nvsHandle);
+        return result;
+    }
+    if (count <= profileNumber) {
+        ESP_LOGE(TAG_CONFIGURATION, "Element %d doesn't exists, configuration only has %d items", profileNumber, count);
+        return ESP_ERR_INVALID_ARG;
+    }
+    // Listing all configuration names (keys) in NVS
+    size_t configurationsSize = 0;
+    result = nvs_get_blob(nvsHandle, NVS_ARRAY, NULL, &configurationsSize);
+    if (result != ESP_OK) {
+        ESP_LOGE(TAG_CONFIGURATION, "Error getting the size of the data array from NVS: %s", esp_err_to_name(result));
+        nvs_close(nvsHandle);
+        return result;
+    }
+    count = configurationsSize / sizeof(profiles);
+    profiles *configurations  = (profiles *)malloc(configurationsSize);
+    if (configurations == NULL) {
+        ESP_LOGE(TAG_CONFIGURATION, "Cannot allocate memory for %zu", count);
+        return ESP_ERR_NO_MEM;
+    }
+    result = nvs_get_blob(nvsHandle, NVS_ARRAY, configurations, &configurationsSize);
+    if (result != ESP_OK) {
+        ESP_LOGE(TAG_CONFIGURATION, "Error getting data array from NVS: %s", esp_err_to_name(result));
+        free(configurations);
+        nvs_close(nvsHandle);
+        return result;
+    }
+    nvs_close(nvsHandle);
+    *profile = configurations[profileNumber];
+    free(configurations);
     return ESP_OK;
 } /**/
--- software/main/configuration.h
+++ software/main/configuration.h
@@ -2,6 +2,7 @@
 #define CONFIGURATION__H
 
 #include <esp_err.h>
+#include <stdint.h>
 
 
 
@@ -9,23 +10,35 @@
 #define TAG_CONFIGURATION           "config"
 #define CONFIG_MAX                  20
 #define CONFIG_LEN_NAME             19
-#define CONFIG_LEN_ADDRESS          30
-#define CONFIG_LEN_OPTION           30
+#define CONFIG_LEN_USERNAME         20
+#define CONFIG_LEN_PASSWORD         40
+#define CONFIG_LEN_LOCAL            20
+#define CONFIG_LEN_REMOTE           40
 
 #define STORAGE_NAMESPACE           "config"
+#define NVS_ARRAY                   "configurations"
+#define NVS_ARRAY_SIZE              "size"
+
+#define TYPE_TCP                    0
+#define TYPE_UDP                    1
+#define TYPE_HTTP                   2
 
 
 typedef struct {
-    char name[CONFIG_LEN_NAME];
-    uint8_t type;
-    char address[CONFIG_LEN_ADDRESS];
-    char option[CONFIG_LEN_OPTION];
+    char name[CONFIG_LEN_NAME];             // Profile name
+    uint8_t type;                           // Profile type (TYPE_*): TCP/UDP/HTTP/BT/BTLE
+    char username[CONFIG_LEN_USERNAME];     // Username for the network (ssid for TCP/IP)
+    char password[CONFIG_LEN_PASSWORD];     // Password for the network (secret for TCP/IP)
+    char local[CONFIG_LEN_LOCAL];           // Local address. on TCP: NULL for DHCP
+    char remote[CONFIG_LEN_REMOTE];         // Remote HOST/URL/..
 } profiles;
 
 
 // Exported functions
 esp_err_t configurationInit();
-esp_err_t configurationList(char names[][CONFIG_LEN_NAME], size_t *count);
-
+esp_err_t configurationList(char names[][CONFIG_LEN_NAME], uint8_t *count);
+esp_err_t configurationClear();
+esp_err_t configurationSave(const profiles *profileArray, uint8_t sizeArray);
+esp_err_t configurationLoad(profiles *profile, uint8_t profileNumber);
 
 #endif
