9c56db3ba2b253405c9ccde0d1c7091fc80e13a5
Ben <andreabenini@users.noreply.github.com>
2025-05-24 00:02:36 +0200

    debug msgs

--- software/main/wifi.c
+++ software/main/wifi.c
@@ -70,9 +70,12 @@ esp_err_t wifiConnect(const char* ssid, const char* password, char *ip) {
 
     // Init wifi and register event handlers
     wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_LOGI(TAG_WIFI, "    - init");
     ESP_RETURN_ON_ERROR(esp_wifi_init(&config), TAG_WIFI, "esp_wifi_init() WiFi init error");
     // ... Register event handlers
+    ESP_LOGI(TAG_WIFI, "    - registering handlers: ESP_EVENT_ANY_ID");
     ESP_RETURN_ON_ERROR(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID,    &wifiEvent, NULL, &instanceAnyID), TAG_WIFI, "Cannot esp_event_handler_instance_register() on ANY_EVENT");
+    ESP_LOGI(TAG_WIFI, "    - registering handlers: IP_EVENT_STA_GOT_IP");
     ESP_RETURN_ON_ERROR(esp_event_handler_instance_register(IP_EVENT,   IP_EVENT_STA_GOT_IP, &wifiEvent, NULL, &instanceGotIP), TAG_WIFI, "Cannot esp_event_handler_instance_register() on GOT_IP");
     
     // Configure WiFi
@@ -95,10 +98,13 @@ esp_err_t wifiConnect(const char* ssid, const char* password, char *ip) {
     // Configure and start WiFi
     // TODO: wifi_country_t wifi_country = {.cc="EU", .schan=1, .nchan=13, .policy=WIFI_COUNTRY_POLICY_AUTO};
     // ESP_RETURN_ON_ERROR( esp_wifi_set_country(&wifi_country) );
+    ESP_LOGI(TAG_WIFI, "    - Setting mode: station");
     ESP_RETURN_ON_ERROR(esp_wifi_set_mode(WIFI_MODE_STA), TAG_WIFI, "esp_wifi_set_mode() Cannot set WiFi station mode");
+    ESP_LOGI(TAG_WIFI, "    - Setting configuration");
     ESP_RETURN_ON_ERROR(esp_wifi_set_config(WIFI_IF_STA, &wifiConfiguration), TAG_WIFI, "esp_wifi_set_config() Cannot setup wifi configuration");
     // DEBUG: Here's the error (sometimes the wifi module hangs up)
     //      - phy_init: saving new calibration data because of checksum failure, mode(0)
+    ESP_LOGI(TAG_WIFI, "    - Starting");
     ESP_RETURN_ON_ERROR(esp_wifi_start(), TAG_WIFI, "Cannot start WiFi");
     
     // XXX: Testing this setting: set maximum power to avoid problems with C3-Super-Mini-Flaw bug
@@ -129,32 +135,33 @@ esp_err_t wifiConnect(const char* ssid, const char* password, char *ip) {
     //      Or plain simple ESP-IDF libraries as usual, reference:
     //      https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/network/esp_wifi.html#_CPPv425esp_wifi_set_max_tx_power6int8_t
     // XXX: Also testing slow tx init with some delays in between
-    vTaskDelay(pdMS_TO_TICKS(1000));
+    ESP_LOGI(TAG_WIFI, "    - Power regulations (8..34)");
+    vTaskDelay(pdMS_TO_TICKS(3000));
     ESP_RETURN_ON_ERROR(esp_wifi_set_max_tx_power(8), TAG_WIFI, "Cannot set WiFi max power to 8");
     vTaskDelay(pdMS_TO_TICKS(3000));
     ESP_RETURN_ON_ERROR(esp_wifi_set_max_tx_power(34), TAG_WIFI, "Cannot set WiFi max power to 34");
 
-    ESP_LOGI(TAG_WIFI, "    WiFi configuration completed");
+    ESP_LOGI(TAG_WIFI, "    - Configuration completed");
     // Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
     // number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler()
     EventBits_t bits = xEventGroupWaitBits(wifiEventGroup, WIFI_BIT_CONNECTED|WIFI_BIT_FAIL, pdFALSE, pdFALSE, portMAX_DELAY);
     strcpy(ip, "");
     /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually happened */
     if (bits & WIFI_BIT_CONNECTED) {
-        ESP_LOGI(TAG_WIFI, "    Connected to AP, SSID:%s", ssid);
+        ESP_LOGI(TAG_WIFI, "    - Connected to AP, SSID:%s", ssid);
         esp_netif_t *netif = esp_netif_get_handle_from_ifkey("WIFI_STA_DEF");
         if (netif) {
             esp_netif_ip_info_t ip_info;
             ESP_RETURN_ON_ERROR(esp_netif_get_ip_info(netif, &ip_info), TAG_WIFI, "Cannot get IP information from the interface");
             sprintf(ip, IPSTR, IP2STR(&ip_info.ip));
         }
-        ESP_LOGI(TAG_WIFI, "    Initialization completed");
+        ESP_LOGI(TAG_WIFI, "    - Initialization completed (%s)", ip);
         result = ESP_OK;
     } else if (bits & WIFI_BIT_FAIL) {
-        ESP_LOGE(TAG_WIFI, "    Failed to connect to SSID:%s", ssid);
+        ESP_LOGE(TAG_WIFI, "    - Failed to connect to SSID:%s", ssid);
         result = ESP_FAIL;
     } else {
-        ESP_LOGE(TAG_WIFI, "    Unknown event: %u", (unsigned int) bits);
+        ESP_LOGE(TAG_WIFI, "    - Unknown event: %u", (unsigned int) bits);
         result = ESP_ERR_INVALID_STATE;
     }
     vEventGroupDelete(wifiEventGroup);
