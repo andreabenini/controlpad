7556d4e7ba396353a822c70c27e6509d45d5e278
Ben <andreabenini@users.noreply.github.com>
2025-06-04 13:12:18 +0200

    Rewriting user profile management with an OOP twist

--- software/main/fsm.profile.c
+++ software/main/fsm.profile.c
@@ -1,38 +1,68 @@
 #include "fsm.profile.h"
-#include "fsm.h"
-#include "configuration.h"
 #include "mainloop.h"
 #include "display.h"
 
 
 extern keyboardStatus keyboard;
 
-typedef struct {
-    uint8_t profileNumber;
-} objProfile;
-objProfile userProfile;
 
-
-void statusProfileInit() {
-    ESP_LOGI(TAG_PROFILE, "Entering in user profile status");
-    displayBackground(LCD_COLOR_BLACK);
+/**
+ * (constructor) Create object profile
+ */
+void* profileCreate(void* objects) {
+    objectProfile* self = (objectProfile*)malloc(sizeof(objectProfile));
+    if (self != NULL) {
+        self->base.type        = STATUS_PROFILE;
+        self->base.init        = profileInit;
+        self->base.eventStatus = profileEventStatus;
+        self->base.eventButton = profileEventButton;
+        self->profileNumber    = UINT8_MAX;
+        self->displayTitle     = (void*)profileDisplayTitle;
+        self->displayStatusBar = (void*)profileDisplayStatusBar;
+        self->displayError     = (void*)profileDisplayError;
+        self->connect          = (void*)profileConnect;
+        self->disconnect       = (void*)profileDisconnect;
+        self->keySend          = (void*)profileKeySend;
+    }
+    return self;
 } /**/
 
-void statusProfileInitProfile(uint8_t number) {
-    // Load required configuration
-    userProfile.profileNumber = number;
-    profiles configuration;
-    char label[CONFIG_LEN_NAME+3];
-    configurationLoad(&configuration, userProfile.profileNumber);
-    // Display useful information to the user
-    sprintf(label, " %-19s", configuration.name);
-    displayTextBackground(0, 0, label, &font8x16, LCD_COLOR_BLACK, LCD_COLOR_WHITE);
-    memcpy(label, configuration.remote, CONFIG_LEN_NAME);
-    label[CONFIG_LEN_NAME] = '\0';
-    displayTextBackground(0, 16, label, &font8x12, LCD_COLOR_WHITE, LCD_COLOR_BLACK);
+
+void profileInit(void* object) {
+    objectProfile* self = object;
+    // Loading profile
+    ESP_LOGI(TAG_PROFILE, "Loading profile [%d]", self->profileNumber);
+    if (self->profileNumber == UINT8_MAX || configurationLoad(&(self->configuration), self->profileNumber) != ESP_OK) {
+        ESP_LOGE(TAG_PROFILE, "Cannot load user profile N:%d", self->profileNumber);
+        statusChange(STATUS_MENU);
+        return;
+    }
+    ESP_LOGI(TAG_PROFILE, "    - '%s', connection type:%d", self->configuration.name, self->configuration.type);
+
+    // Display information on screen
+    self->displayTitle(self);
+    self->displayStatusBar(self);
+    // Peer connection and display information
+    if (self->connect(self) != ESP_OK) {
+        statusChange(STATUS_MENU);
+        return;
+    }
+}
+
+void profileDisplayTitle(objectProfile* self) {
+    char line[CONFIG_LEN_NAME+3];
+    displayBackground(LCD_COLOR_BLACK);
+    sprintf(line, " %-19s", self->configuration.name);              // Profile name
+    displayTextBackground(0, 0, line, &font8x16, LCD_COLOR_BLACK, LCD_COLOR_WHITE);
+    memcpy(line, self->configuration.remote, CONFIG_LEN_NAME);      // Remote host address
+    line[CONFIG_LEN_NAME] = '\0';
+    displayTextBackground(0, 16, line, &font8x12, LCD_COLOR_WHITE, LCD_COLOR_BLACK);
     displayBox(0, 116, LCD_WIDTH, LCD_HEIGHT, LCD_COLOR_WHITE);
+}
+
+void profileDisplayStatusBar(objectProfile* self) {
     char type[5];
-    switch (configuration.type) {
+    switch (self->configuration.type) {
         case TYPE_TCP:
             strcpy(type, "TCP");
             break;
@@ -42,32 +72,22 @@ void statusProfileInitProfile(uint8_t number) {
         case TYPE_HTTP:
             strcpy(type, "HTTP");
             break;
+        case TYPE_BT:
+            strcpy(type, "BT");
+            break;
+        case TYPE_BTLE:
+            strcpy(type, "BTLE");
+            break;
         default:
             strcpy(type, "????");
     }
     displayText(LCD_WIDTH-(strlen(type)+1)*8, LCD_HEIGHT-12, type, &font8x12, LCD_COLOR_BLACK);
-    // Network connection
-    if (configuration.type <= TYPE_HTTP) {              // TCP, UDP, HTTP
-        displayTextBackground(10, LCD_HEIGHT-12*2-2, "connecting...", &font8x12, LCD_COLOR_WHITE, LCD_COLOR_BLACK);
-        if (wifiConnect(configuration.username, configuration.password, label) != ESP_OK) {
-            statusProfileInitError(" WIFI  ERROR ",
-                                   " CONNECTION  ",
-                                   "   FAILED    ");
-            wifiDisconnect();
-            return;
-        }
-        displayTextBackground(10, 55, "C O N N E C T E D", &font8x16, LCD_COLOR_WHITE, LCD_COLOR_BLACK);
-        // displayTextBackground(10, 55, "  DISCONNECTED ! ", &font8x16, LCD_COLOR_WHITE, LCD_COLOR_BLACK);
-        displayTextBackground(10, LCD_HEIGHT-12*2-2, label, &font8x12, LCD_COLOR_WHITE, LCD_COLOR_BLACK);
-        displayText(10, LCD_HEIGHT-12,     "IP",      &font8x12, LCD_COLOR_BLACK);
-    }
-} /**/
-
+}
 
 /**
- * Init error message Box
+ * display message box error
  */
-void statusProfileInitError(char *line1, char *line2, char *line3) {
+void profileDisplayError(objectProfile* self, char *line1, char *line2, char *line3) {
     // Waiting window
     displayBackground(LCD_COLOR_RED);
     displayWindow(LCD_WIDTH/10, LCD_HEIGHT/4, LCD_WIDTH/10*8, LCD_HEIGHT/2, LCD_COLOR_BLACK, LCD_COLOR_WHITE, 2);
@@ -75,55 +95,92 @@ void statusProfileInitError(char *line1, char *line2, char *line3) {
     displayTextBackground(26, 55, line2, &font8x12, LCD_COLOR_BLACK, LCD_COLOR_WHITE);
     displayTextBackground(26, 70, line3, &font8x12, LCD_COLOR_BLACK, LCD_COLOR_WHITE);
     vTaskDelay(pdMS_TO_TICKS(5000));
-    statusChange(STATUS_MENU);
 } /**/
 
 
-void statusProfileEvent() {
-    // ESP_LOGI(TAG_PROFILE, "I2C "                    // FIXME: Convert it to a Display function
-    //     "A[%d%d%d%d%d%d%d%d] "
-    //     "B[%d%d%d%d%d%d%d%d]  "
-    //     "[%4d,%4d] [%4d,%4d]",
-    //     keyboard.button[0].stateCurrent,        // A0..7, B0..7 I2C buttons
-    //     keyboard.button[1].stateCurrent,
-    //     keyboard.button[2].stateCurrent,
-    //     keyboard.button[3].stateCurrent,
-    //     keyboard.button[4].stateCurrent,
-    //     keyboard.button[5].stateCurrent,
-    //     keyboard.button[6].stateCurrent,
-    //     keyboard.button[7].stateCurrent,
-    //     keyboard.button[8].stateCurrent,
-    //     keyboard.button[9].stateCurrent,
-    //     keyboard.button[10].stateCurrent,
-    //     keyboard.button[11].stateCurrent,
-    //     keyboard.button[12].stateCurrent,
-    //     keyboard.button[13].stateCurrent,
-    //     keyboard.button[14].stateCurrent,
-    //     keyboard.button[15].stateCurrent,
-    //     keyboard.joystick1_X, keyboard.joystick1_Y,
-    //     keyboard.joystick2_X, keyboard.joystick2_Y
-    // );
+/**
+ * Connect to remote device, configuration dependant
+ */
+esp_err_t profileConnect(objectProfile* self) {
+    displayTextBackground(10, LCD_HEIGHT-12*2-2, "connecting...", &font8x12, LCD_COLOR_WHITE, LCD_COLOR_BLACK);
+    self->socket = -1;
+    if (self->configuration.type <= TYPE_HTTP) {                // TCP, UDP, HTTP
+        // WiFi connection
+        displayText(10, LCD_HEIGHT-12, "IP", &font8x12, LCD_COLOR_BLACK);
+        if (wifiConnect(self->configuration.username, self->configuration.password, self->configuration.local) != ESP_OK) {
+            self->disconnect(self);
+            self->displayError(self,
+                               " WIFI ERROR, ",
+                               " CONNECTION  ",
+                               "   FAILED    ");
+            return ESP_ERR_WIFI_NOT_INIT;
+        }
+        displayTextBackground(10, LCD_HEIGHT-12*2-2, self->configuration.local, &font8x12, LCD_COLOR_WHITE, LCD_COLOR_BLACK);
+
+        // Create TCP socket to remote
+        self->socket = tcpSocket(self->configuration.remote);
+        if (self->socket < 0) {
+            self->disconnect(self);
+            self->displayError(self,
+                               "   CANNOT    ",
+                               " CONNECT TO  ",
+                               " REMOTE HOST ");
+            return ESP_ERR_WIFI_NOT_CONNECT;
+        }
+        displayTextBackground(10, 55, "C O N N E C T E D", &font8x16, LCD_COLOR_WHITE, LCD_COLOR_BLACK);
+        // displayTextBackground(10, 55, "  DISCONNECTED ! ", &font8x16, LCD_COLOR_WHITE, LCD_COLOR_BLACK);
+        return ESP_OK;
+    }
+    self->displayError(self,
+            " CONNECTION  ",
+            "TYPE NOT YET ",
+            " IMPLEMENTED ");
+    return ESP_ERR_NOT_SUPPORTED;
+} /**/
+
+
+/**
+ * Network disconnection
+ */
+esp_err_t profileDisconnect(objectProfile* self) {
+    wifiDisconnect();
+    displayTextBackground(10, 55, "                 ", &font8x16, LCD_COLOR_WHITE, LCD_COLOR_BLACK);
+    return ESP_OK;
+} /**/
+
+
+
+void profileEventStatus(void* object) {
 } /**/
 
 /**
  * USER PROFILE keypress event, return to menu when START/SYSTEM button is pressed
  */
-void statusProfileKeypress(uint8_t button, bool status) {
-    // DEBUG: Am I reaching this one ???
-    ESP_LOGW(TAG_PROFILE, "statusProfileKeypress() Button: %d,  Status: %d", button, status);
-    if (button==B_START) {
-        ESP_LOGW(TAG_PROFILE, "statusProfileKeypress() Status: %d", status);
-    }
-    // DEBUG: END
-    if (button==B_START && status==1) {
-        ESP_LOGI(TAG_PROFILE, "Closing user profile status, returning to main menu");
-        wifiDisconnect();
-        statusChange(STATUS_MENU);
+void profileEventButton(void* object, uint8_t button, bool status) {
+    objectProfile* self = object;
+    ESP_LOGI(TAG_PROFILE, "eventButton() Button: %d, status: %d", button, status);    // DEBUG:
+    if (status==1) {
+        if (button==B_START) {
+            ESP_LOGI(TAG_PROFILE, "Closing user profile status, returning to main menu");
+            self->disconnect(self);
+            statusChange(STATUS_MENU);
+        } else {
+            self->keySend(self, button);
+        }
     }
 } /**/
 
+/**
+ * Send a key to remote
+ */
+esp_err_t profileKeySend(objectProfile *self, uint8_t button) {
+    ESP_LOGW(TAG_PROFILE, "keySend() Button: %d", button);
+    tcpDataSend(self->socket, "Hello\n");
+    return ESP_OK;
+} /**/
 
 
+// TODO: Code for parsing regexp strings
 /*
 #include <stdio.h>
 #include <stdlib.h>
--- software/main/fsm.profile.h
+++ software/main/fsm.profile.h
@@ -8,20 +8,44 @@
 #include <stdbool.h>
 #include "driver/uart.h"                                        // IWYU pragma: keep
 
-
+// Project includes
 #include "wifi.h"                                               // IWYU pragma: keep
+#include "fsm.h"
+#include "configuration.h"
 
 #define TAG_PROFILE             "profile"
 
 
 /**
- * FSM object properties
+ * FSM profile object properties
  */
-void statusProfileInit();
-void statusProfileInitProfile(uint8_t number);
-void statusProfileInitError(char *line1, char *line2, char *line3);
-void statusProfileShow();
-void statusProfileEvent();
-void statusProfileKeypress(uint8_t button, bool status);
+typedef struct {
+    statusObject base;                                          // Base object
+    method       displayTitle;                                  // display header bar in profile view
+    method       displayStatusBar;                              // display status bar in profile view
+    void         (*displayError)(void* self, char *line1, char *line2, char *line3);
+    esp_err_t    (*connect)(void* self);                        // Connection to the remote network
+    esp_err_t    (*disconnect)(void* self);                     // Disconnection from the remote network
+    esp_err_t    (*keySend)(void* self, uint8_t button);        // Connection to the remote network
+
+    uint8_t      profileNumber;                                 // Selected profile number from the list
+    profiles     configuration;                                 // Current configuration loaded
+    int          socket;                                        // TCP socket (when TCP/HTTP is used)
+} objectProfile;
+
+
+// Object base methods
+void* profileCreate(void* objects);                             // Constructor
+void  profileInit(void* object);                                // init on statusChange()
+void  profileEventStatus(void* object);
+void  profileEventButton(void* object, uint8_t button, bool status);
+
+// Object methods
+void  profileDisplayTitle(objectProfile* self);
+void  profileDisplayStatusBar(objectProfile* self);
+void  profileDisplayError(objectProfile* self, char *line1, char *line2, char *line3);
+esp_err_t profileConnect(objectProfile* self);
+esp_err_t profileDisconnect(objectProfile* self);
+esp_err_t profileKeySend(objectProfile* self, uint8_t button);
 
 #endif
